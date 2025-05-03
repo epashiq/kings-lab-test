92c0d4203e79ae4f1830c1591fb74145289a2390
epashiq <ashiqashi4715@gmail.com>
2025-05-03 12:17:18 +0530

    product model created and create a getproduct method

--- lib/controller/services/product_services.dart
+++ lib/controller/services/product_services.dart
@@ -1,7 +1,19 @@
 import 'package:dio/dio.dart';
+import 'package:kings_lab_test/model/product_model.dart';
 
 class ProductServices {
   final dio = Dio();
 
-  
+  Future<ProductModel> getProducts() async {
+    try {
+      final resp = await dio.get('https://dummyjson.com/products');
+      if (resp.statusCode == 200) {
+        return ProductModel.fromJson(resp.data);
+      } else {
+        throw Exception('faild to load products');
+      }
+    } catch (e) {
+      throw Exception('faild to load products');
+    }
+  }
 }
